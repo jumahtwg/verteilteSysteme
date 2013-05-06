45b53a955c217ccbe6e2b5463c3f2ee563a026ea
Mathieu <marisserqhtwg-online.de>
2013-05-06 13:47:02 +0200

    blatt4

---
+++ src/blatt4/BullyProcess.java
@@ -0,0 +1,23 @@
+package blatt4;
+
+public class BullyProcess extends Process {
+
+
+	public BullyProcess(int id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startElection() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
